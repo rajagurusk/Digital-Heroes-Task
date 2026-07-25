6848a36ff9616c207af9a2af5585e42e4aa0980d
rajagurusk <rajagurusivakumar1@example.com>
2026-07-25 16:11:06 +0530

    tested

--- client/src/components/LeadForm.jsx
+++ client/src/components/LeadForm.jsx
@@ -56,11 +56,12 @@ function LeadForm() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/leads", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/leads`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(formData),
+});
+
 
       const data = await res.json();
 
